7678e810cda5aeacfb31bb9d667f87b0e984abc2
3-abhishekh <abhisheky@kychub.com>
2025-02-06 12:02:29 +0530

    center aligned

--- src/Components/UserInput.tsx
+++ src/Components/UserInput.tsx
@@ -19,7 +19,7 @@ const UserInput = () => {
         <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }} >
             <h1>"Cat Facts Generator</h1>
             <h2>Discover interesting facts about cats!"</h2>
-            <form onSubmit={onFormSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: "0.5rem" }}  >
+            <form onSubmit={onFormSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems:"center", gap: "0.5rem" }}  >
                 <label htmlFor="noOfFacts">“How many facts about cats do you want to see?”</label>
                 <input type="number" id="noOfFacts" name="noOfFacts" />
                 <input type="submit" value="Submit" style={{ maxWidth: 200 }} />
