c6098d8917ea6aabb2adbb5f5e3731b86273af60
3-abhishekh <abhisheky@kychub.com>
2025-02-06 11:55:26 +0530

    added favicon and heading, introduced loading

--- src/Components/ResultComponent.tsx
+++ src/Components/ResultComponent.tsx
@@ -8,12 +8,13 @@ const ResultComponent = () => {
 
     console.log("noOfFacts", noOfFacts);
 
-    const [result, setResult] = useState<{ fact: string, length: number }>({ fact: "NA", length: 2 })
+    const [result, setResult] = useState<{ fact: string, length: number, isLoading: boolean }>({ fact: "NA", length: 2, isLoading: false })
 
     useEffect(() => {
         const fetchData = async () => {
+            setResult(prev => ({ ...prev, isLoading: true }))
             const response = await axios("https://catfact.ninja/fact")
-            setResult(response?.data)
+            setResult(({ ...response?.data, isLoading: false }))
         }
         fetchData()
     }, [])
@@ -21,11 +22,11 @@ const ResultComponent = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }} >
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "0.5rem" }} >
-                <button onClick={() => navigate("/")} >Go Back</button>
-                <h1>"Cat Facts Generator</h1>
+                <button onClick={() => navigate("/")} style={{marginTop:"10px"}} >Go Back</button>
+                <h1>Cat Facts Generator</h1>
             </div>
             <h2>Discover interesting facts about cats!"</h2>
-            <p>{result?.fact}</p>
+            <p>Fact: {result?.isLoading ? "Loading..." : result?.fact}</p>
         </div>
     )
 }
